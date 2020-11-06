c6ce6558944c16c69672479aaed6e5b956bdc1a5
HK2Clutch <ghousia101@hotmail.com>
2020-11-05 22:09:13 -0500

    slingshot

--- Slingshot.js
+++ Slingshot.js
@@ -6,16 +6,22 @@ class SlingShot{
             stiffness: 0.04,
             length: 10
         }
+        this.pointB = pointB;
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
     }
-
+    fly(){
+        this.sling.bodyA = null;
+    }
     display(){
-        var pointA = this.sling.bodyA.position;
-       // var pointB = this.pointB;
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+        var pointB = this.pointB;
         strokeWeight(4);
         console.log(pointB);
-        line(pointA.x, pointA.y,this.pointB.x, this.pointB.y);
+        line(pointA.x, pointA.y,pointB.x, pointB.y); 
+        }
+       
     }
     
 }
